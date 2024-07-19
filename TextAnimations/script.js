28fee974639450581e71527795f9692be2d53931
omkushwaha9 <omkushwaha2005@gmail.com>
2024-07-19 15:21:38 +0530

    TextAnimations

--- TextAnimations/script.js
+++ TextAnimations/script.js
@@ -1,5 +1,35 @@
-var h1text = document.querySelector("h1").textContent
+function breakTheText() {
+    var h1 = document.querySelector("h1")
+    var h1Text = h1.textContent
 
-var splittedText = h1Text.split("")
+    var splittedText = h1Text.split("")
+    var halfValue = splittedText.length / 2
 
-var clutter = ""
+    var clutter = ""
+
+    splittedText.forEach(function (elem, idx) {
+        if (idx < halfValue) {
+            clutter += `<span class="a">${elem}</span>`
+        } else {
+            clutter += `<span class="b">${elem}</span>`
+        }
+    })
+       h1.innerHTML = clutter
+}
+
+breakTheText()
+
+gsap.from("h1 .a",{
+    y:80,
+    duration:0.6,
+    delay:0.5,
+    stagger:0.15,
+    opacity:0
+})
+gsap.from("h1 .b",{
+    y:80,
+    duration:0.6,
+    delay:0.5,
+    stagger:-0.15,
+    opacity:0
+})
